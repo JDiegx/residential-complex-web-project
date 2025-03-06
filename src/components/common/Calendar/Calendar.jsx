f3b5deb7751a9d56d6b031ca970d6cba3111f55d
Juan Diego Sanchez <juandiegosanchex21@gmail.com>
2025-03-05 19:49:12 -0500

    Update: 05/03/2025

---
+++ src/components/common/Calendar/Calendar.jsx
@@ -0,0 +1,38 @@
+import React, { useState, useEffect } from 'react'
+import "./Calendar.css"
+import { calendar } from '../../../database/data'
+
+const Calendar = () => {
+    const [days, setDays] = useState(calendar[0])
+    const [hours, setHours] = useState(calendar[1])
+
+    useEffect(() => {
+        setDays(calendar[0])
+        setHours(calendar[1])
+    })
+
+    return (
+        <table className='calendar-table'>
+            <thead className='calendar-table__table-head'>
+                {
+                    days.map((day) => {
+                        return (
+                            <tr className='calendar-table__days' key={day}>
+                                <th> {day}</th>
+                            </tr>
+                        )
+                    })
+                }
+            </thead>
+            <tbody>
+                {
+                    hours.map((hour) => <tr key={hour} >
+                        <th>{hour}</th>
+                    </tr>)
+                }
+            </tbody>
+        </table>
+    )
+}
+
+export default Calendar
---
+++ src/components/pages/Home/Home.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import "./Home.css"
+import Calendar from '../../common/Calendar/Calendar'
+
+const Home = () => {
+    return (
+        <div>
+            <p>Home</p>
+            <Calendar />
+        </div>
+    )
+}
+
+export default Home;
