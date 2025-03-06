6ffc509940ef13b0cbf0a4faab40262bed21a51d
Juan Diego Sanchez <juandiegosanchex21@gmail.com>
2025-03-06 11:03:21 -0500

    Update 06/03/2025

--- src/components/pages/Home/Home.jsx
+++ src/components/pages/Home/Home.jsx
@@ -1,10 +1,11 @@
-import React from 'react'
+import React, { useState } from 'react'
 import "./Home.css"
+import Calendar from "../../common/Calendar/Calendar.jsx"
 
 const Home = () => {
     return (
         <div>
-            <p>Home</p>
+            <Calendar />
         </div>
     )
 }
