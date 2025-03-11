1d40468004a40a43055a05a6c145a411bd4e0d68
JDiegx <juandiegosanchex21@gmail.com>
2025-03-10 19:00:16 -0500

    Update: 10/03/2025

--- src/components/pages/Home/Home.jsx
+++ src/components/pages/Home/Home.jsx
@@ -1,13 +1,21 @@
-import React, { useState } from 'react'
-import "./Home.css"
-import Calendar from "../../common/Calendar/Calendar.jsx"
+import React from 'react';
+import "./Home.css";
 
 const Home = () => {
     return (
-        <div>
-            <p>Home</p>
-        </div>
-    )
-}
+        <section className='home'>
+            <div className='home__content'>
+                <p className='home__description'>
+                    Exclusive access for residents <br /> and their guests.
+                </p>
+                <h1 className='home__title'>
+                    Make a reservation and enjoy the exclusive dishes of our residential complex.
+                </h1>
+                <button className='home__button'>Explore available spoods</button>
+            </div>
+            <div className='home__image'></div>
+        </section>
+    );
+};
 
 export default Home;
