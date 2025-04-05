2c5b1702e647735695d3d92c9af22933464edbac
JDiegx <juandiegosanchex21@gmail.com>
2025-04-05 10:58:01 -0500

    Update zones and images

--- src/components/common/Zone/Zone.jsx
+++ src/components/common/Zone/Zone.jsx
@@ -1,11 +1,10 @@
 import React from 'react'
 import "./Zone.css"
-import betaImage from '/img/1.png'
 
 const Zone = ({ zone }) => {
     return (
         <div className='zone'>
-            <img className='zone__image' src={betaImage} alt={`Image of ${zone.name}`} />
+            <img className='zone__image' src={`/img/zones/${zone.image}`} alt={`Image of ${zone.name}`} />
             <div className='zone__information'>
                 <p className='zone__capacity'>{zone.capacity}</p>
                 <h2 className='zone__title'>{zone.name}</h2>
