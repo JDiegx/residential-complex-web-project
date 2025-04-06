43d782f3b1fd5ca2ada4dbd361ff5aac78772a87
JDiegx <juandiegosanchex21@gmail.com>
2025-04-06 12:17:18 -0500

    Update2: 06/04/2025

--- src/components/pages/Home/Home.jsx
+++ src/components/pages/Home/Home.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import "./Home.css";
 import SecurityAndAccess from '../../common/SecurityAndAccess/SecurityAndAccess';
 import Header from '../../common/Header/Header.jsx'
@@ -6,6 +6,22 @@ import { zones } from '../../../data/data.js';
 import Zone from '../../common/Zone/Zone.jsx';
 
 const Home = () => {
+    const zonesRef = useRef(null); // Referencia al contenedor con scroll
+
+    const scrollLeft = () => {
+        zonesRef.current.scrollBy({
+            left: -300,
+            behavior: 'smooth'
+        });
+    };
+
+    const scrollRight = () => {
+        zonesRef.current.scrollBy({
+            left: 300,
+            behavior: 'smooth'
+        });
+    };
+
     return (
         <>
             <Header />
@@ -24,7 +40,7 @@ const Home = () => {
             <SecurityAndAccess />
             <section className="home__zones-section">
                 <h2 className="home__zones-title">Explore available spoods:</h2>
-                <div className="home__zone-container">
+                <div className="home__zone-container" ref={zonesRef}>
                     <div className="home__zones">
                         {
                             zones.map((zone) => <Zone key={zone.id} zone={zone} />)
@@ -32,12 +48,12 @@ const Home = () => {
                     </div>
                 </div>
                 <div className="home__zones-navigation">
-                    <button className="home__zones-button">
+                    <button className="home__zones-button" onClick={scrollLeft} aria-label="Scroll left">
                         <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="home__zones-icon" viewBox="0 0 16 16">
                             <path fillRule="evenodd" d="M12 8a.5.5 0 0 1-.5.5H5.707l2.147 2.146a.5.5 0 0 1-.708.708l-3-3a.5.5 0 0 1 0-.708l3-3a.5.5 0 1 1 .708.708L5.707 7.5H11.5a.5.5 0 0 1 .5.5" />
                         </svg>
                     </button>
-                    <button className="home__zones-button">
+                    <button className="home__zones-button" onClick={scrollRight} aria-label="Scroll right">
                         <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="home__zones-icon" viewBox="0 0 16 16">
                             <path fillRule="evenodd" d="M4 8a.5.5 0 0 1 .5-.5h5.793L8.146 5.354a.5.5 0 1 1 .708-.708l3 3a.5.5 0 0 1 0 .708l-3 3a.5.5 0 0 1-.708-.708L10.293 8.5H4.5A.5.5 0 0 1 4 8" />
                         </svg>
