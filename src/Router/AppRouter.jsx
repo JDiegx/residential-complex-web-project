3050338404c576653eafd5f0253ac299c8c18d24
JDiegx <juandiegosanchex21@gmail.com>
2025-04-28 17:17:16 -0500

    Update: 28/04/2025

--- src/Router/AppRouter.jsx
+++ src/Router/AppRouter.jsx
@@ -1,12 +1,13 @@
 import React from 'react'
 import { Routes, Route } from 'react-router-dom'
-import Header from '../components/common/Header/Header.jsx'
+import Footer from '../components/common/Footer/Footer.jsx'
 import Home from '../components/pages/Home/Home.jsx'
 import Dashboard from '../components/pages/Dashboard/Dashboard.jsx'
 import Booking from '../components/pages/Booking/Booking.jsx'
 import NotFound from '../components/pages/NotFound/NotFound.jsx'
 import ReservationForm from '../components/common/ReservationForm/ReservationForm.jsx'
 import ReservationCalendar from '../components/common/ReservationCalendar/ReservationCalendar.jsx'
+import Beta from '../components/common/Beta/Beta.jsx'
 
 const AppRouter = () => {
     return (
@@ -18,8 +19,10 @@ const AppRouter = () => {
                 <Route path='/booking' element={<Booking />} />
                 <Route path='/reservation-form' element={<ReservationForm />} />
                 <Route path='/reservation-calendar' element={<ReservationCalendar />} />
+                <Route path='/beta' element={<Beta />} />
                 <Route path='*' element={<NotFound />} />
             </Routes>
+            {/* <Footer /> */}
         </>
     )
 }
